141d447391c24b7ad15ed6ec704e4a09f033d5da
AyushDevHub <coder.ayushshaw@gmail.com>
2025-07-01 12:15:06 +0530

    added left-aligned-pyramid

--- Understanding_Patterns/LeftAlignedPyramid.java
+++ Understanding_Patterns/LeftAlignedPyramid.java
@@ -1,18 +1,18 @@
 package Understanding_Patterns;
-
 import java.util.Scanner;
-public class square_pattern {
+public class LeftAlignedPyramid {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Square Pattern");
-        System.out.println("Enter the number of rows for the square pattern:"); // Taking input from user
+        System.out.println("Left Aligned Half Pyramid");
+        System.out.print("Enter the number of rows for the left aligned half pyramid: "); // Taking input from user
         int n = sc.nextInt();
-        for(int i = 0; i < n; i++) { // Loop for each row
-            for(int j = 0; j < n; j++) { // Loop for each column in the row
+                for(int i = 1; i <= n; i++) { // Loop for each row
+            for(int j = 1; j <= i; j++) { // Loop for each column in the row
                 System.out.print(" *"); // Printing a star followed by a space
             }
             System.out.println(); // Move to the next line after printing all columns in the current row
         }
         sc.close();
     }
+    
 }
